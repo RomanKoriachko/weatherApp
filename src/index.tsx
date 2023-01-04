58385c07ac268359ed09241cd8df72906018ace3
Roman <mazaxaka.tyt@gmail.com>
2023-01-04 15:32:39 +0200

    add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './container/App/App'
 import { store } from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 )
